2df46b84850d6162d4f26cb51b7d4832183a9146
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-29 00:02:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 class Artist
-  extend Memorable, Findable
-  include Paramable
-
+  # extend Memorable, Findable
+  # include Paramable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :songs
 
